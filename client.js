ea5113b357eec97eaed23f72460a05f94f7cde30
Red John <redjohn367@gmail.com>
2014-03-02 09:13:39 -0700

    Added documentation
    * Added better documentation
    * Formatted for docco

--- client.js
+++ client.js
@@ -1,43 +1,73 @@
+//# Client Examples
+//
+//Examples of using the http library to make web requests
+
+// ## Imports
+// We need the `http` library to make the requests, and `querystring`
+// provides some functions to help with creating queries. Both of
+// these are port of node's standard library.
 var http = require('http')
   , qs = require('querystring');
 
-// Sugar provides some function that make changing the opts a bit
-// simpler
+// Sugar provides some functions that make working with objects a
+// bit simpler. We use these methods mostly for modifying `opts`
+// to use with the different requests.`
 require('sugar');
 Object.extend();
 
+// ## Response Handlers
+// Since everything in node.js is async, we'll need to provide some
+// callback functions for the http requests.
+
+// ### Error Handler
 // Logs an error in the request to the console
 var errorHandler = function(error) {
   console.log('ERROR: ' + error.message);
 };
 
-// Logs the response to the console, displaying the status, headers,
-// and body
+// ### Response Handler
+// This function will get called with the server's response as the argument,
+// and will log it to the console, displaying the status, headers, and body
 var responseHandler = function(res) {
+  // Log some info about the response
   console.log('STATUS: ' + res.statusCode);
   console.log('HEADERS: ' + JSON.stringify(res.headers));
   console.log('BODY: ');
+
+  // Make sure we use the right encoding to read the data from the response
   res.setEncoding('utf8');
   res.on('data', function (chunk) {
+    // Log the response body
     console.log(chunk);
   });
 };
 
-// This is the data that node needs to make the request
+// ## Making HTTP requests
+// Creating a request is fairly simple. We just need to use the `http.request`
+// function, which needs 2 arguments, the options for the request, like the host
+// and method, and then a callback function to use when the request completes.
+//
+// After we create the request, we want to add a callback that will be used in
+// case there is an error during the request.
+//
+// When everything is set up how we want, we just need to call the `end` method
+// on the request, which will tell node to make the request and process it with
+// our callbacks.
+
+// ### Request Options
+// These options configure the request to point to our [test server](server.html).
 var opts = {
   hostname: '127.0.0.1',
   port: 1337,
   method: 'GET'
 };
 
-// *** GET requests ***
-
-// Make a GET request
+// ### GET request
 var getReq = http.request(opts, responseHandler);
 getReq.on('error', errorHandler);
 getReq.end();
 
-// Make a GET request with a query string
+// ### GET Request with query string
 var query = {name: 'value'};
 var getWithQuery = http.request(
   opts.clone().merge({path: '/?' + qs.stringify(query)}),
@@ -46,11 +76,9 @@ var getWithQuery = http.request(
 getWithQuery.on('error', errorHandler);
 getWithQuery.end();
 
-// *** POST requests ***
-
-// Make a POST request
+// ### POST with request body
 var postReq = http.request(opts.clone().merge({method: 'POST'}), responseHandler);
 postReq.on('error', errorHandler);
-postReq.write("name=value");
+postReq.write(qs.stringify(query));
 postReq.end();
 
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-/*
-A simple web server you can run to test making requests. It processes
-GET AND POST requests, returning info about any data you posted in the
-response body. You can use this to test requests to make sure you're
-submitting the correct data
-*/
+// # Test Server
+// A simple web server you can run to test making requests. It processes
+// GET AND POST requests, returning info about any data you posted in the
+// response body. You can use this to test requests to make sure you're
+// submitting the correct data
 
+// ## Imports
+// We need the `http` library to create the server, and `url`
+// has some functions for parsing a query string.
 var http = require('http')
   , url = require('url');
 
-// Helper functions for displaying the request data
+// ## Helper Functions
+// These functions are used for turning the request data into a
+// string that we'll return in the response body.
+
+// Takes the data from a POST request's body and creates a string
+// to display its contents. Checks to see if the body contains key/value
+// pairs; if not, it's treated as a raw string.
 var dumpPostData = function(data) {
   var dataStr = "";
   var pairs = data.split("&");
@@ -20,6 +28,9 @@ var dumpPostData = function(data) {
   return dataStr;
 };
 
+// Takes a request body that does not contain key/value pairs and returns
+// it formatted for the response. Properly handles the case where the body
+// was empty.
 var dumpRequestBody = function(body) {
   var dataStr = "";
   if (body === null || body === "") {
@@ -30,6 +41,8 @@ var dumpRequestBody = function(body) {
   return dataStr;
 };
 
+// Takes an array of strings containing a single key/value pair (e.g.,
+// "key=value") and returns it formatted for the response.
 var dumpPairs = function(pairs) {
   var dataStr = "Key-Value Pairs:\r\n";
   for (var i = 0; i < pairs.length; ++i) {
@@ -38,29 +51,47 @@ var dumpPairs = function(pairs) {
   return dataStr;
 };
 
+// ## Creating the Server
+// `http.createServer` needs a function that takes 2 arguments, an object
+// containing the request info and an object containing the response info.
+// Whenever a request is received, it will call this function and then
+// return the response after it's done.
 http.createServer(function (req, res) {
+  // Handle a GET request
   if (req.method === "GET") {
+    // Indicate that the response was a success and that we're going to
+    // return some text.
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Your GET was received\r\n');
 
+    // Check for a query string, and if there is one, display its contents
     var query = url.parse(req.url).query;
     if (query === null || query === "") {
       res.end();
     } else {
       res.end(dumpPairs(query.split('&')));
     }
+  // Handle a POST request
   } else if (req.method === "POST") {
+    // Indicate that the response was a success and that we're going to
+    // return some text.
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
     var postData = "";
-    // Read in the data from the POST body
+    // Read in the data from the request's body. Since IO in node is async,
+    // we need to do this with a callback function. The request will trigger
+    // a `data` event whenever there is a chunk of data to read, so we'll use
+    // a function here that will read each chunk of data and combine them all.
     req.on('data', function(chunk) {
       postData += chunk;
     });
 
-    // When all the data is read, send the response
+    // When all the data is read, the request will trigger and `end` event. We
+    // set up a callback for this event that will take all the data we read and
+    // send it back in the response.
     req.on('end', function() {
       res.write("Your POST was received\r\n");
+      // Format the data to make it more user-friendly
       res.end(dumpPostData(postData));
     });
   }
